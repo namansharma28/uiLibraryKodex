7e47d5cc8fb2d5fa21c748cef91a7398511c0ba6
Siddhant Mul <siddhantmul18@gmail.com>
2026-05-08 23:47:00 +0530

    feat: create accordion component skeleton

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,25 @@
+
+export const Accordion = () => {
+  return (
+    <div>Accordion</div>
+  )
+}
+
+export const AccordionItem = () => {
+  return (
+    <div>AccordionItem</div>
+  )
+}
+
+export const AccordionTitle = () => {
+  return (
+      <div>AccordionTitle</div>
+  )
+}
+
+export const AccordionContent = () => {
+  return (
+    <div>AccordionContent</div>
+  )
+}
+
