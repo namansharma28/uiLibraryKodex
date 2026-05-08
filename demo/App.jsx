859b0b2f04c466f09f0affef48360e2852a312fe
Siddhant Mul <siddhantmul18@gmail.com>
2026-05-09 00:12:18 +0530

    feat: add accordion component structure and content

--- demo/App.jsx
+++ demo/App.jsx
@@ -1,43 +1,77 @@
 import {
   Button,
 } from '../src';
+import { Accordion, AccordionContent, AccordionItem, AccordionTitle } from '../src/components/Accordion/Accordion';
 
 function App() {
+
+  const accordionData = [
+    {
+      id: 1,
+      title: "What is React?",
+      content:
+        "React is a JavaScript library used to build fast and interactive user interfaces.",
+    },
+    {
+      id: 2,
+      title: "What is Tailwind CSS?",
+      content:
+        "Tailwind CSS is a utility-first CSS framework used for rapidly building modern websites.",
+    },
+    {
+      id: 3,
+      title: "What is an Accordion?",
+      content:
+        "An accordion is a UI component that allows users to expand and collapse sections of content.",
+    },
+  ];
+
   return (
-    <div className="max-w-7xl mx-auto p-8">
-      <h1 className="text-4xl font-bold mb-8 text-primary-600">
-        UI Library Demo
-      </h1>
+    <>
+      <div className="max-w-7xl mx-auto p-8">
+        <h1 className="text-4xl font-bold mb-8 text-primary-600">
+          UI Library Demo
+        </h1>
 
-      <section className="mb-12">
-        <h2 className="text-3xl font-semibold mb-4">Buttons</h2>
-        <div className="flex gap-4 flex-wrap items-center mb-6">
-          <Button variant="primary">Primary Button</Button>
-          <Button variant="secondary">Secondary Button</Button>
-          <Button variant="outline">Outline Button</Button>
-          <Button variant="ghost">Ghost Button</Button>
-        </div>
+        <section className="mb-12">
+          <h2 className="text-3xl font-semibold mb-4">Buttons</h2>
+          <div className="flex gap-4 flex-wrap items-center mb-6">
+            <Button variant="primary">Primary Button</Button>
+            <Button variant="secondary">Secondary Button</Button>
+            <Button variant="outline">Outline Button</Button>
+            <Button variant="ghost">Ghost Button</Button>
+          </div>
 
-        <h3 className="text-xl font-medium mt-6 mb-3 text-gray-600">
-          Button Sizes
-        </h3>
-        <div className="flex gap-4 flex-wrap items-center mb-6">
-          <Button size="sm">Small</Button>
-          <Button size="md">Medium</Button>
-          <Button size="lg">Large</Button>
-        </div>
+          <h3 className="text-xl font-medium mt-6 mb-3 text-gray-600">
+            Button Sizes
+          </h3>
+          <div className="flex gap-4 flex-wrap items-center mb-6">
+            <Button size="sm">Small</Button>
+            <Button size="md">Medium</Button>
+            <Button size="lg">Large</Button>
+          </div>
 
-        <h3 className="text-xl font-medium mt-6 mb-3 text-gray-600">
-          Disabled State
-        </h3>
-        <div className="flex gap-4 flex-wrap items-center">
-          <Button disabled>Disabled Button</Button>
-        </div>
-      </section>
-      <section>
+          <h3 className="text-xl font-medium mt-6 mb-3 text-gray-600">
+            Disabled State
+          </h3>
+          <div className="flex gap-4 flex-wrap items-center">
+            <Button disabled>Disabled Button</Button>
+          </div>
+        </section>
+        <section>
 
-      </section>
-    </div>
+        </section>
+
+        <Accordion>
+          {accordionData.map(({ id, title, content }) => (
+            <AccordionItem key={id}>
+              <AccordionTitle>{title}</AccordionTitle>
+              <AccordionContent>{content}</AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+    </>
   );
 }
 
--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,25 +1,42 @@
+import { ChevronDown } from "lucide-react"
+import { cn } from "../../utils/cn"
 
-export const Accordion = () => {
-  return (
-    <div>Accordion</div>
-  )
+export const Accordion = ({ children, className }) => {
+    return (
+        <div className={cn("max-w-lg w-full divide-y divide-zinc-300 min-h-96", className)}>
+            {children}
+        </div>
+    )
 }
 
-export const AccordionItem = () => {
-  return (
-    <div>AccordionItem</div>
-  )
+export const AccordionItem = ({ children, className }) => {
+    return (
+        <div className={cn(className)}>{children}</div>
+    )
 }
 
-export const AccordionTitle = () => {
-  return (
-      <div>AccordionTitle</div>
-  )
+export const AccordionTitle = ({ children, className }) => {
+    return (
+        <button
+            className={cn("flex items-center justify-between w-full py-1.5 cursor-pointer font-medium hover:underline decoration-1", className)}
+        >
+            {children}
+            <span>
+                <ChevronDown className={cn("w-5 h-5 text-zinc-500")} />
+            </span>
+        </button>
+    )
 }
 
-export const AccordionContent = () => {
-  return (
-    <div>AccordionContent</div>
-  )
+export const AccordionContent = ({ children, className }) => {
+    return (
+        <div
+            className={cn(className)}
+        >
+            <div>
+                <div className="pb-1.5">{children}</div>
+            </div>
+        </div>
+    )
 }
 
