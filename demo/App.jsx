d8b566b8f9017d73ae81b2ef1732596d7b113913
Abhay-333 <abhayd0331@gmail.com>
2026-05-16 17:18:43 +0530

    create aspect ratio component

--- demo/App.jsx
+++ demo/App.jsx
@@ -1,6 +1,7 @@
 import {
   Button,
 } from '../src';
+import { AspectRatio } from "../src";
 
 function App() {
   return (
@@ -34,7 +35,104 @@ function App() {
           <Button disabled>Disabled Button</Button>
         </div>
       </section>
+      
+      {/* Aspect Ratio */}
       <section>
+        <h1 className="text-3xl font-semibold mb-4">Aspect Ratio</h1>
+        
+        <div className="flex gap-6 flex-wrap">
+          <div className="flex items-center flex-col">
+          <AspectRatio ratio={16 / 9}>
+            <img
+              src="https://images.unsplash.com/photo-1773332611516-93826171cef2?q=80&w=687&auto=format&fit=crop"
+              alt="Photo"
+              className="h-full w-full object-cover rounded-lg"
+            />
+          </AspectRatio>
+          <h3 className="text-lg font-medium mt-6 mb-3 text-gray-600">Widescreen Aspect Ratio(Youtube)</h3>
+        </div>
+        
+        <div className="flex items-center flex-col">
+          <AspectRatio ratio={1 / 1}>
+            <img
+              src="https://images.unsplash.com/photo-1773332611516-93826171cef2?q=80&w=687&auto=format&fit=crop"
+              alt="Photo"
+              className="h-full w-full object-cover rounded-lg"
+            />
+          </AspectRatio>
+          <h3 className="text-lg font-medium mt-6 mb-3 text-gray-600">Square Aspect Ratio(Instagram Post)</h3>
+        </div>
+        
+        
+        <div className="flex items-center flex-col">
+          <AspectRatio ratio={4 / 3}>
+            <img
+              src="https://images.unsplash.com/photo-1773332611516-93826171cef2?q=80&w=687&auto=format&fit=crop"
+              alt="Photo"
+              className="h-full w-full object-cover rounded-lg"
+            />
+          </AspectRatio>
+          <h3 className="text-lg font-medium mt-6 mb-3 text-gray-600">Standard / Fullscreen Aspect Ratio</h3>
+        </div>
+        
+        <div className="flex items-center flex-col">
+          <AspectRatio ratio={9 / 16}>
+            <img
+              src="https://images.unsplash.com/photo-1773332611516-93826171cef2?q=80&w=687&auto=format&fit=crop"
+              alt="Photo"
+              className="h-full w-full object-cover rounded-lg"
+            />
+          </AspectRatio>
+          <h3 className="text-lg font-medium mt-6 mb-3 text-gray-600">Vertical / Portrait Aspect Ratio</h3>
+        </div>
+        
+        <div className="flex items-center flex-col">
+          <AspectRatio ratio={21 / 9}>
+            <img
+              src="https://images.unsplash.com/photo-1773332611516-93826171cef2?q=80&w=687&auto=format&fit=crop"
+              alt="Photo"
+              className="h-full w-full object-cover rounded-lg"
+            />
+          </AspectRatio>
+          <h3 className="text-lg font-medium mt-6 mb-3 text-gray-600">Ultrawide Aspect Ratio</h3>
+        </div>
+
+        
+        <div className="flex items-center flex-col">
+          <AspectRatio ratio={3 / 2}>
+            <img
+              src="https://images.unsplash.com/photo-1773332611516-93826171cef2?q=80&w=687&auto=format&fit=crop"
+              alt="Photo"
+              className="h-full w-full object-cover rounded-lg"
+            />
+          </AspectRatio>
+          <h3 className="text-lg font-medium mt-6 mb-3 text-gray-600">Classic Photography Aspect Ratio</h3>
+        </div>
+        
+        <div className="flex items-center flex-col">
+          <AspectRatio ratio={5 / 4}>
+            <img
+              src="https://images.unsplash.com/photo-1773332611516-93826171cef2?q=80&w=687&auto=format&fit=crop"
+              alt="Photo"
+              className="h-full w-full object-cover rounded-lg"
+            />
+          </AspectRatio>
+          <h3 className="text-lg font-medium mt-6 mb-3 text-gray-600">Traditional Monitor Aspect Ratio</h3>
+        </div>
+        
+        <div className="flex items-center flex-col">
+          <AspectRatio ratio={2 / 1}>
+            <img
+              src="https://images.unsplash.com/photo-1773332611516-93826171cef2?q=80&w=687&auto=format&fit=crop"
+              alt="Photo"
+              className="h-full w-full object-cover rounded-lg"
+            />
+          </AspectRatio>
+          <h3 className="text-lg font-medium mt-6 mb-3 text-gray-600">Univisium Aspect Ratio(Used in some modern cinematic content)</h3>
+        </div>
+
+
+        </div>
 
       </section>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './styles/globals.css';
 
 // Components
 export { Button } from './components/Button';
+export { AspectRatio } from './components/Aspect Ratio';
 
 // Utils
 export { cn } from './utils/cn';
