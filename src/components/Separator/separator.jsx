e6ce8337ca1f01208239664c0e875100fd79f1c1
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-06-03 15:38:16 +0530

    feat: add separator component

--- src/components/Separator/separator.jsx
+++ src/components/Separator/separator.jsx
@@ -1,1 +1,31 @@
-export { Separator } from './Separator';
+import { cn } from '@utils/cn';
+import PropTypes from 'prop-types';
+
+export const Separator = ({
+  orientation = 'horizontal',
+  className,
+}) => {
+  return (
+    <div
+      role="separator"
+      className={cn(
+        'shrink-0 bg-zinc-200 dark:bg-zinc-700',
+
+        orientation === 'horizontal'
+          ? 'h-px w-full'
+          : 'h-full w-px',
+
+        className
+      )}
+    />
+  );
+};
+
+Separator.propTypes = {
+  orientation: PropTypes.oneOf([
+    'horizontal',
+    'vertical',
+  ]),
+  className: PropTypes.string,
+};
+
